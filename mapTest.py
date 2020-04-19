d77319e6a094a46a5dce0e4310d59339df4b33cf
gabrielagustin <gabiagus@gmail.com>
2020-04-19 19:50:31 -0300

    improving code

--- mapTest.py
+++ mapTest.py
@@ -1,107 +1,87 @@
-import pandas as pd
+#!/usr/bin/python
 
+'''
+Created on Tuesday, January 28th 2020, 2:31:18 pm
+Author: Gabriel Garcia
+
+Copyright (c) 2020 Your Company
+'''
 
 import glob
-
-import geopandas as gpd
-# import matplotlib.pyplot as plt
+import pandas as pd
+from netCDF4 import Dataset
 import numpy as np
-# from matplotlib import cm
-# from mpl_toolkits.axes_grid1 import make_axes_locatable
-# from shapely.geometry import Point, Polygon
 
 import plotly.express as px
 
-import functions
-
-# pathIn = "/media/ggarcia/data/Satellogic/Satellogic/Data/Neuquen/Sentinel-5p-L3_CH4(Methane)/Max/"
-# pathOut = "/media/ggarcia/data/Satellogic/Satellogic/Data/Neuquen/Sentinel-5p-L3_CH4(Methane)/PNG/Max/"
-
-
-pathIn = "/media/ggarcia/data/Satellogic/Satellogic/Data/Neuquen/Sentinel-5p-L3_CH4(Methane)/Min/"
-pathOut = "/media/ggarcia/data/Satellogic/Satellogic/Data/Neuquen/Sentinel-5p-L3_CH4(Methane)/PNG/Min/"
-
-# pathIn = "/media/ggarcia/data/Satellogic/Satellogic/Data/Neuquen/Sentinel-5p-L3_CH4(Methane)/Mean/"
-# pathOut = "/media/ggarcia/data/Satellogic/Satellogic/Data/Neuquen/Sentinel-5p-L3_CH4(Methane)/PNG/Mean/"
-
-
-# se listan todos los archivos .tif que pertenecen a las imagenes SAR Sentinel 1
-files = sorted([f for f in glob.glob(pathIn + "**/*.tif", recursive=True)])
-
-
-# path al geojson del pais
-
-argentina_fp = "/media/ggarcia/data/Satellogic/Satellogic/Data/SIG_Argentina/provincia.json"
-provincias = gpd.read_file(argentina_fp)
-
-print(type(provincias))
-
-
-###  La Pampa, Neuquén, Río Negro, Chubut, Santa Cruz y Tierra del Fuego, Antártida e Islas del Atlántico Sur
-patagonia = [ 'Neuquén'] ### 'Mendoza', 'Chubut', 'Santa Cruz', 'La Pampa', 'Río Negro'
-
-map_dep1 = provincias[provincias.nam.isin(patagonia)]
-
-gridCRS = map_dep1.crs
-print(gridCRS)
-
-for i in range(0, 1):
-    
-    # fig, ax = plt.subplots(1)
-    print(files[i])
-    name = files[i]
-    ### for min and max
-    date = name[87:-8]
-    ### for mean
-    # date = name[88:-9]
-    print("date: "+str(date))
-
-    src_ds, band, GeoT, Project = functions.openFileHDF(files[i], 1)
-
-    # print(Project)
-    xmin,xmax,ymin,ymax=GeoT[0],GeoT[0]+GeoT[1]*src_ds.RasterXSize,GeoT[3]+GeoT[5]*src_ds.RasterYSize,GeoT[3]
-
-    # cmap = cm.colors.ListedColormap(['royalblue', 'cyan',
-    #                                   'yellow', 'orange'])
-    # cmap.set_over('red')
-    # cmap.set_under('blue')
-
-    # maxV = []
-    # maxV.append(np.nanmax(band))
-    # minV = []
-    # minV.append(np.nanmin(band))
-
-
-    # fig, ax = plt.subplots(figsize = (15,15))
-    # plt.title('XCH4    '+'Date: ' + str(date))
-    # ## Units: ppmv (parts per million by volume)
-    # im = ax.imshow(band, extent=[xmin,xmax,ymin,ymax], vmin=1700, vmax=1850, cmap=plt.get_cmap('jet'), interpolation='nearest', aspect='auto')
-    # divider = make_axes_locatable(ax)
-    # cax = divider.append_axes("right", size="5%", pad=0.05)
-    # fig.colorbar(im, cax=cax, label='[ppmv]')
-
-    ## capital de Neuquen
-
-    # lat_point = [-38.98583333]
-    # lon_point = [-68.06416667]
-    # point_geom = Point(zip(lon_point, lat_point))
-    # crs = {'init': 'epsg:4326'}
-    # point = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[point_geom])
-    # point.plot(ax=ax, marker='o', color='blue', markersize=20)
-
-
-    # map_dep1.plot(ax=ax, alpha=0.2, edgecolor='k')
-    
-    
-    fig = px.imshow(band)
-    # fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
-                            # color_discrete_sequence=["fuchsia"], zoom=3, height=900)
-    fig.update_layout(mapbox_style="open-street-map")
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    fig.show()
 
 
 
-    
-    
-    # plt.show()
+
+def plot_NC_file(pathIn, pathOut):
+    """ 
+    Parameters:
+    -----------
+    pathIn : path of the folder where is netCDF files
+    pathOut : path of the folder where .png files will be created
+    Returns: 
+    --------
+    """
+
+    # se listan todos los archivos .tif que pertenecen a las imagenes SAR Sentinel 1
+    files = sorted([f for f in glob.glob(pathIn + "*.nc", recursive=True)])
+
+
+    for i in range(0, 1):
+        # print(files[i])
+
+        nc_f = files[i]
+        # nc_f = path + netcd_fname
+        
+        # print(nc_f)
+        
+        date = files[i][106:-58]
+        print("date:" + str(date))
+
+        ### Dataset is the class behavior to open the file
+        ### and create an instance of the ncCDF4 class    
+        nc_file = Dataset(nc_f, 'r')  
+
+        ### para ver el el contenido del archivo .nc en diferentes niveles
+        # print (nc_file.groups)
+        # print (nc_file.groups['PRODUCT'].variables.keys())
+        # print (nc_file.groups['PRODUCT'].variables[ 'methane_mixing_ratio'])
+
+        lons = nc_file.groups['PRODUCT'].variables['longitude'][:][0,:,:]
+        lats = nc_file.groups['PRODUCT'].variables['latitude'][:][0,:,:]
+        
+        XCH4 = nc_file.groups['PRODUCT'].variables['methane_mixing_ratio'][0,:,:]
+
+
+        d = {'XCH4': np.array(XCH4).flatten(), 'lat': np.array(lats).flatten(), 'lon': np.array(lons).flatten()}
+
+        df = pd.DataFrame(data=d)   
+        
+        df = df[(df['XCH4'] >= 1300) & (df['XCH4'] <=2100 )]
+
+        fig = px.scatter_mapbox(df, lat="lat", lon="lon", hover_name="XCH4", hover_data=["XCH4"],
+                                 color="XCH4", color_continuous_scale=px.colors.cyclical.IceFire, zoom=3, height=900)
+        
+        fig.update_layout(mapbox_style="open-street-map")
+        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        fig.show()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    ### Path of netCDF file
+    pathIn = "/media/ggarcia/data/Satellogic/Satellogic/Data/Neuquen/Sentinel-5p-L3_CH4(Methane)/NC/"
+    pathOut = "/media/ggarcia/data/Satellogic/Satellogic/Data/Neuquen/Sentinel-5p-L3_CH4(Methane)/NC/PNG/"
+    plot_NC_file(pathIn, pathOut)
+
+
